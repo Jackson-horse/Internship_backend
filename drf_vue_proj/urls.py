128e5e6822c7e580fb935cb5b5126ed40bb6a7e4
Jackson <Jackson_m30@outlook.com>
2022-09-19 19:19:17 +0800

    little changes

--- drf_vue_proj/urls.py
+++ drf_vue_proj/urls.py
@@ -8,7 +8,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('account.urls')),
-    path('student/',include('student_predict.urls')),
+    #path('student/',include('student.urls')),
     path('api/token/',TokenObtainPairView.as_view()),
     path('api/token/refresh',TokenRefreshView.as_view())
 ]
