2469570cbc2d26c538eaec1225763ec4565f5395
Jackson <Jackson_m30@outlook.com>
2022-09-20 16:52:28 +0800

    CORS configuration

--- drf_vue_proj/settings.py
+++ drf_vue_proj/settings.py
@@ -147,7 +147,7 @@ SIMPLE_JWT = {
 }
 
 # CORS
-RIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = (
 	'DELETE',
 	'GET',
@@ -158,6 +158,6 @@ CORS_ALLOW_METHODS = (
 	'VIEW',
 )
 CORS_ALLOW_HEADERS = (
-	'Authorization',
+	'authorization',
 	'content-type',
 )
